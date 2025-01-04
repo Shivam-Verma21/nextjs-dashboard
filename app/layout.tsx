381293a7aa0d0181b75ed7d5ff85e9278ac34b93
Shivam Verma <shivamverma21201@gmail.com>
2025-01-04 17:32:07 +0530

    Optimizing Fonts and Images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import '@/app/ui/global.css';
+// import './ui/global.css';
+
+// import {inter} from '@/app/ui/fonts';
+
+import {nunito} from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${nunito.className} antialiased`}>{children}</body>
     </html>
   );
 }
